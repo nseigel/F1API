6d22e06ed2a08a18adddd92a37b91583c68b3bcc
nseigel <105315630+nseigel@users.noreply.github.com>
2024-06-18 12:37:29 -0600

    adding visualize segment function (working!)

--- draw_segment.py
+++ draw_segment.py
@@ -2,11 +2,13 @@ import base64
 import zlib
 import json
 from turtle import *
+import datetime
+import time
 
 import requests
 
-url = 'http://livetiming.formula1.com/static/2024/' \
-      '2024-06-09_Canadian_Grand_Prix/2024-06-09_Race/' \
+url = 'http://livetiming.formula1.com/static/2023/' \
+      '2023-07-30_Belgian_Grand_Prix/2023-07-30_Race/' \
       'Position.z.jsonStream'
 resp = requests.get(url)
 
@@ -37,40 +39,62 @@ def scale(data, factor):
         data[i] = data[i] * factor
     return data
 
+def convert_time(timestamp):
+    timestamp = timestamp[0:26]
+    timestamp = timestamp.replace('Z', '0')
+    while len(timestamp) < 26:
+        timestamp = timestamp + '0'
+    timestamp = timestamp + '+00:00'
+    time = datetime.datetime.fromisoformat(timestamp)
+    return time
+
 def get_driver_data(driver_number, start_index, end_index, factor):
     x_data = []
     y_data = []
+    timestamps = []
     for i in range(start_index,end_index):
         random_row = resp.text.split('\r\n')[i]
         timing, data, _ = random_row.split('"')
         decoded_data = zlib.decompress(base64.b64decode(data), -zlib.MAX_WBITS)
         decoded_data = json.loads(decoded_data)
-        max_data = decoded_data["Position"][0]["Entries"][str(driver_number)]
-        x_data.append(max_data['X'])
-        y_data.append(max_data['Y'])
+        timestamp = convert_time(decoded_data["Position"][0]["Timestamp"])
+        timestamps.append(timestamp)
+        driver_data = decoded_data["Position"][0]["Entries"][str(driver_number)]
+        x_data.append(driver_data['X'])
+        y_data.append(driver_data['Y'])
     abs_min, abs_max = min_max_value(x_data, y_data)
     x_data_processed = scale(normalize(x_data, abs_min, abs_max), factor)
     y_data_processed = scale(normalize(y_data, abs_min, abs_max), factor)
-    return(x_data_processed, y_data_processed)
+    return(x_data_processed, y_data_processed, timestamps)
 
-def draw_segment(start_time, end_time, driver_number, scale, colour, vizualize):
-    driver_x, driver_y = get_driver_data(driver_number, start_time, end_time, scale)
+def draw_segment(start_time, end_time, driver_number, scale, colour, visualize):
+    driver_x, driver_y, timestamps = get_driver_data(driver_number, start_time, end_time, scale)
     t.color(colour)
     t.penup()
     t.goto(driver_x[0], driver_y[0])
     t.pendown()
-
     for i in range(len(driver_x)):
         t.goto(driver_x[i], driver_y[i])
+
     t.penup()
     if visualize:
         t.goto(driver_x[0], driver_y[0])
-        t.shape(circle)
+        #t.shape('circle')
+        prev_timestamp = 0
+        for i in range(len(driver_x)):
+            timestamp = timestamps[i]
+            if i != 0:
+                time_delta = timestamp - prev_timestamp
+                total_delta = time_delta.microseconds + time_delta.seconds * 1000000
+                time.sleep(total_delta/1000000)
+            t.goto(driver_x[i], driver_y[i])
+            prev_timestamp = timestamp
 
 screen = Screen()
 screen.screensize(50, 50)
 t = Turtle()
 
-draw_segment(5000, 5130, 55, 300, "red", False)
+#draw_segment(5000, 5130, 55, 300, "red", False)
+draw_segment(5000, 5115, 44, 300, "red", True)
 
 screen.exitonclick()
