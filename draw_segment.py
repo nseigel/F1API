2a4d72f00c39b0d556431a407e310bd34c0c77f8
nseigel <105315630+nseigel@users.noreply.github.com>
2024-06-28 10:32:04 -0600

    gitignore and making easier to reuse code

--- draw_segment.py
+++ draw_segment.py
@@ -4,6 +4,7 @@ import json
 from turtle import *
 import datetime
 import time
+import normalise as n
 
 import requests
 
@@ -11,33 +12,6 @@ url = 'http://livetiming.formula1.com/static/2024/' \
       '2024-05-26_Monaco_Grand_Prix/2024-05-26_Race/' \
       'Position.z.jsonStream'
 resp = requests.get(url)
-
-def min_max_value(x_data, y_data):
-    mins = [min(x_data), min(y_data)]
-    abs_min = min(mins)
-    maxes = [max(x_data), max(y_data)]
-    abs_max = max(maxes)
-    return(abs_min, abs_max)
-
-def normalize(data, abs_min, abs_max):
-    data_length = len(data)
-    negative_values = abs_min < 0
-    data_range = abs_max - abs_min
-    if negative_values:
-        correction_factor = abs(abs_min)
-        for i in range(data_length):
-            data[i] = data[i] + correction_factor
-    for i in range(data_length):
-        if data_range == 0:
-            data[i] = 0
-        else:
-            data[i] = data[i]/data_range
-    return data
-
-def scale(data, factor):
-    for i in range(len(data)):
-        data[i] = data[i] * factor
-    return data
 
 def convert_time(timestamp):
     timestamp = timestamp[0:26]
@@ -63,10 +37,8 @@ def get_driver_data(driver_number, start_index, end_index, factor):
             driver_data = decoded_data["Position"][x]["Entries"][str(driver_number)]
             x_data.append(driver_data['X'])
             y_data.append(driver_data['Y'])
-    abs_min, abs_max = min_max_value(x_data, y_data)
-    x_data_processed = scale(normalize(x_data, abs_min, abs_max), factor)
-    y_data_processed = scale(normalize(y_data, abs_min, abs_max), factor)
-    return(x_data_processed, y_data_processed, timestamps)
+    processed_data = n.normalize([x_data, y_data], factor)
+    return(processed_data[0], processed_data[1], timestamps)
 
 def draw_segment(start_time, end_time, driver_number, scale, colour, visualize):
     driver_x, driver_y, timestamps = get_driver_data(driver_number, start_time, end_time, scale)
---
+++ normalise.py
@@ -0,0 +1,31 @@
+def min_max_value(data):
+    mins = []
+    maxes = []
+    for group in data:
+        mins.append(min(group))
+        maxes.append(max(group))
+    
+    return(min(mins), max(maxes))
+
+def scale(data, factor):
+    for group in data:
+        for i in range(len(group)):
+            group[i] = group[i] * factor
+    return data
+
+def normalize(data, factor):
+    abs_min, abs_max = min_max_value(data)
+    negative_values = abs_min < 0
+    data_range = abs_max - abs_min
+    correction_factor = abs(abs_min)
+    for group in data:
+        if negative_values:
+            for i in range(len(group)):
+                group[i] += correction_factor
+        for i in range(len(group)):
+            if data_range == 0:
+                group[i] = 0
+            else:
+                group[i] = group[i]/data_range
+    data = scale(data, factor)
+    return data
